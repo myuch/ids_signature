94576a6c9237ca1cb6e7149fd1f58e045940a204
Ivan <yaroshivan7@gmail.com>
2021-10-19 13:45:09 +0300

    Second commit

--- themes/idssignature/template-parts/home-tabs/tab-1.php
+++ themes/idssignature/template-parts/home-tabs/tab-1.php
@@ -1,5 +1,5 @@
 <!-- template-parts/home-tabs/tab-1.php -->
-<div id="tab1" class="slide">
+<div id="tab1" class="slide sldfgdflgfdkjgn">
     <di class="intro">
         <h1>Scroll Horizontally1</h1>
         <p>Use `scrollHorizontally: true` and enable the mouse wheel horizontal scroll for slides.</p>
