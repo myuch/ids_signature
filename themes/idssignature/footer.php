d62d34b7b6aa992cc5fc30181180790b130fd0c7
Maksym Yurchenko <35823610+myuch@users.noreply.github.com>
2021-10-19 19:04:05 +0300

    2 slides

--- themes/idssignature/footer.php
+++ themes/idssignature/footer.php
@@ -28,6 +28,10 @@
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap" rel="stylesheet">
 <?php wp_footer(); ?>
 
 </body>
--- themes/idssignature/template-parts/home-tabs/tab-1.php
+++ themes/idssignature/template-parts/home-tabs/tab-1.php
@@ -1,7 +1,11 @@
 <!-- template-parts/home-tabs/tab-1.php -->
-<div id="tab1" class="slide sldfgdflgfdkjgn">
-    <di class="intro">
-        <h1>Scroll Horizontally1</h1>
-        <p>Use `scrollHorizontally: true` and enable the mouse wheel horizontal scroll for slides.</p>
-    </di>
+<div id="tab1" class="slide">
+  <div class="slide_bg">
+    <img src="/wp-content/themes/idssignature/assets/images/slide1_bg.jpg" alt="">
+  </div>
+  <div class="container">
+    <h1>Signature Designs for Your Unique Space</h1>
+    <a href="#">SCROLL TO NAVIGATE</a>
+    <p>Brought to you by the same creative minds behind IDS Kids</p>
+  </div>
 </div>
