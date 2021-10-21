5f198db94bb478b0bcd81b023e21a2aeca19b383
Maksym Yurchenko <35823610+myuch@users.noreply.github.com>
2021-10-21 16:58:32 +0300

    tab 5

--- themes/idssignature/header.php
+++ themes/idssignature/header.php
@@ -199,7 +199,14 @@
 
 
 	<!-- Gallery Modal -->
-	<div id="galleryModal" style="display: block;" class="galleryModal">
+	<div id="galleryModal" class="galleryModal" style="display:none;">
+		<svg id="hamburger-close-white" xmlns="http://www.w3.org/2000/svg" width="27" height="26" viewBox="0 0 27 26" style="display: inline;">
+			<g>
+				<g>
+					<path d="M2.533.6L13.5 11.568 24.467.601a1 1 0 1 1 1.414 1.413L14.914 12.981l10.954 10.955a1 1 0 1 1-1.414 1.413L13.5 14.395 2.546 25.349a1 1 0 1 1-1.414-1.413L12.086 12.98 1.12 2.014A1 1 0 1 1 2.533.601z"></path>
+				</g>
+			</g>
+		</svg>
 		<div class="galleryModal_wrap">
 			<span class="galleryModal-title">
 				Signage & Outdoor Landmarks
--- themes/idssignature/src/js/app.js
+++ themes/idssignature/src/js/app.js
@@ -25,46 +25,57 @@ jQuery(document).ready(function ($) {
         $('.modalMenu').fadeOut();
     });
 
-    var slideCount = $('.galleryModal_slider_item').length;
-
-    $('.galleryModal_slider_wrap').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false,
-        arrows: true,
-        prevArrow: $('#galleryModal_arrowPrev'),
-        nextArrow: $('#galleryModal_arrowNext'),
+    $('#hamburger-close-white').on('click', function () {
+      $('#galleryModal').hide();
     });
 
-    $('.slider-nav-thumbnails').slick({
-        slidesToShow: slideCount,
-        slidesToScroll: 1,
-        asNavFor: '.galleryModal_slider_wrap',
-        dots: false,
-        focusOnSelect: true,
-        centerMode: true,
-        centerPadding: '0px',
-    });
-
-    // Remove active class from all thumbnail slides
-    $('.slider-nav-thumbnails .slick-slide').removeClass('slick-active');
-
-    // Set active class to first thumbnail slides
-    $('.slider-nav-thumbnails .slick-slide').eq(0).addClass('slick-active');
-
-    // On before slide change match active thumbnail to current slide
-    $('.galleryModal_slider_wrap').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-        var mySlideNumber = nextSlide;
-        $('.slider-nav-thumbnails-item').removeClass('slick-active');
-        $('.slider-nav-thumbnails-item').eq(mySlideNumber).addClass('slick-active');
-   });
-
-   $('.slider-nav-thumbnails-item').on('click', function(){
-        $('.slider-nav-thumbnails-item').removeClass('slick-active');
-        $(this).addClass('slick-active');
-   });
-
-
+    slick_gallery_init();
 
 });
+
+function slick_gallery_init(){
+  var slideCount = $('.galleryModal_slider_item').length;
+
+  $('.galleryModal_slider_wrap').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: false,
+      arrows: true,
+      prevArrow: $('#galleryModal_arrowPrev'),
+      nextArrow: $('#galleryModal_arrowNext'),
+  });
+
+  // $('.slider-nav-thumbnails').slick({
+  //     slidesToShow: slideCount,
+  //     slidesToScroll: 1,
+  //     asNavFor: '.galleryModal_slider_wrap',
+  //     dots: false,
+  //     focusOnSelect: true,
+  //     centerMode: true,
+  //     centerPadding: '0px',
+  // });
+
+  // Remove active class from all thumbnail slides
+  $('.slider-nav-thumbnails .slick-slide').removeClass('slick-active');
+
+  // Set active class to first thumbnail slides
+  $('.slider-nav-thumbnails .slick-slide').eq(0).addClass('slick-active');
+
+  // On before slide change match active thumbnail to current slide
+  $('.galleryModal_slider_wrap').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+      var mySlideNumber = nextSlide;
+      $('.slider-nav-thumbnails-item').removeClass('slick-active');
+      $('.slider-nav-thumbnails-item').eq(mySlideNumber).addClass('slick-active');
+ });
+
+ $('.slider-nav-thumbnails-item').on('click', function(){
+      $('.slider-nav-thumbnails-item').removeClass('slick-active');
+      $(this).addClass('slick-active');
+ });
+}
+function slick_gallery_unslick() {
+  $('.galleryModal_slider_wrap').slick('unslick');
+}
+window.slick_gallery_init = slick_gallery_init;
+window.slick_gallery_unslick = slick_gallery_unslick;
